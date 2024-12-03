5ea801487b581ccf7119125c4c16e59d26184724
Meghanapr3424 <meghanareddypr34@gmail.com>
2024-12-03 18:52:21 +0530

    Fix bcrypt deployment issues

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db.js');
+const bcrypt = require('bcryptjs');
+
 
 dotenv.config();
 connectDB();
